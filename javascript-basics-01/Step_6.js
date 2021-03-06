f4e955adce18331f236cbf43fa600dd8dcafc791
abd <62624790+abdbadgithub@users.noreply.github.com>
2021-03-06 13:33:23 +0200

    step6-js

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,10 @@
+document.getElementById("validate").addEventListener("click", display);
+
+function display() {
+  var first_number = document.getElementById("first_number").value;
+  var second_number = document.getElementById("second_number").value;
+
+  var total = parseFloat(first_number) / parseFloat(second_number);
+
+  alert("the total is :" + total);
+}
