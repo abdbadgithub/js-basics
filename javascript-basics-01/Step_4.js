86f300298696412551a54ce9182d038634be1f3f
abd <62624790+abdbadgithub@users.noreply.github.com>
2021-03-06 11:48:26 +0200

    step4-js

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,7 @@
+var name = prompt("please enter your first name", "");
+var surname = prompt("please enter your surname", "");
+var city = prompt("please enter your city name", "");
+
+if (name != null && surname != null && city != null) {
+  alert("name : " + name + "\n surname : " + surname + "\n city : " + city);
+}
