f83d97b025494e8714fb9039fca0632c95ce979c
abd <62624790+abdbadgithub@users.noreply.github.com>
2021-03-06 13:56:54 +0200

    step8-js

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,11 @@
+document.getElementById("validate").addEventListener("click", display);
+
+function display() {
+  var age = document.getElementById("age").value;
+
+  if (age < 18) {
+    alert("you are under 18");
+  } else {
+    alert("you are over 18");
+  }
+}
