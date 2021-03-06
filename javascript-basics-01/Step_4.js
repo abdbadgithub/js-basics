197e9edd33d2f95d9eb1259e6625456c6a85e6d8
abd <62624790+abdbadgithub@users.noreply.github.com>
2021-03-06 13:29:45 +0200

    step5-js

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,7 +1,20 @@
+/* first solution
 var name = prompt("please enter your first name", "");
 var surname = prompt("please enter your surname", "");
 var city = prompt("please enter your city name", "");
 
 if (name != null && surname != null && city != null) {
   alert("name : " + name + "\n surname : " + surname + "\n city : " + city);
+}
+*/
+
+document.getElementById("validate").addEventListener("click", display);
+
+function display() {
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var city = document.getElementById("city").value;
+
+  if (name != null && surname != null && city != null)
+    alert("name : " + name + "\n surname : " + surname + "\n city : " + city);
 }
