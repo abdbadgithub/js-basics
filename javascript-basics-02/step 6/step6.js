c357750157d6107e1979dbd9bbe824333621e939
abd <62624790+abdbadgithub@users.noreply.github.com>
2021-03-07 18:00:53 +0200

    step6-js

---
+++ javascript-basics-02/step 6/step6.js
@@ -0,0 +1,26 @@
+function change(v) {
+  if (v == "img1") {
+    document.getElementById("image1").src = "images/image1_2.jpg";
+  } else if (v == "img2") {
+    document.getElementById("image2").src = "images/image2_2.jpg";
+  } else if (v == "img3") {
+    document.getElementById("image3").src = "images/image3_2.jpg";
+  } else if (v == "img4") {
+    document.getElementById("image4").src = "images/image4_2.jpg";
+  } else if (v == "img5") {
+    document.getElementById("image5").src = "images/image5_2.jpg";
+  }
+}
+function reset(v) {
+  if (v == "img1") {
+    document.getElementById("image1").src = "images/image1.jpg";
+  } else if (v == "img2") {
+    document.getElementById("image2").src = "images/image2.jpg";
+  } else if (v == "img3") {
+    document.getElementById("image3").src = "images/image3.jpg";
+  } else if (v == "img4") {
+    document.getElementById("image4").src = "images/image4.jpg";
+  } else if (v == "img5") {
+    document.getElementById("image5").src = "images/image5.jpg";
+  }
+}
